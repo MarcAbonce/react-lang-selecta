9246ded70ab0ce43e6cde0ec92a9c59d55ca6781
Marc Abonce Seguin <marc-abonce@mailbox.org>
2023-06-14 23:45:44 -0600

    move json to react lib's src

--- py-src/build_locale_regions_json.py
+++ py-src/build_locale_regions_json.py
@@ -7,13 +7,15 @@ import babel
 locale_regions = {}
 for locale in babel.localedata.locale_identifiers():
     locale_parts = locale.split('_')
-    if len(locale_parts) > 1 and re.match(r'[A-Z]{2}', locale_parts[-1]):
+    if len(locale_parts) > 1 and re.match(r'^[A-Z]{2}$', locale_parts[-1]):
         lang = locale_parts[0]
         region = locale_parts[-1]
         if lang not in locale_regions:
-            locale_regions[lang] = [region]
-        else:
-            locale_regions[lang].append(region)
+            name = babel.Locale(lang).language_name.title()
+            locale_regions[lang] = { 'name': name, 'regions': [region] }
+        elif region not in locale_regions[lang]['regions']:
+            locale_regions[lang]['regions'].append(region)
 
-with open('./locale_regions.json', 'w+') as f:
+
+with open('../src/data/locale_regions.json', 'w+') as f:
     json.dump(locale_regions, f)
