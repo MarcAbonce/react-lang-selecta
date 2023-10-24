19ffc7fd4d6e00925ba52c3664eb1e420ea1467c
Marc Abonce Seguin <marc-abonce@mailbox.org>
2023-10-23 23:30:37 -0600

    fix linter

--- src/utils/getFlagEmoji.ts
+++ src/utils/getFlagEmoji.ts
@@ -5,13 +5,11 @@
  * Yes, even this file. 🚨🏴‍☠🚨
  */
 
-/* eslint-disable object-property-newline */
 const REGIONAL_INDICATOR_ALPHABET: Record<string, string> = {
   A: '🇦', B: '🇧', C: '🇨', D: '🇩', E: '🇪', F: '🇫', G: '🇬', H: '🇭', I: '🇮', J: '🇯',
   K: '🇰', L: '🇱', M: '🇲', N: '🇳', O: '🇴', P: '🇵', Q: '🇶', R: '🇷', S: '🇸', T: '🇹',
   U: '🇺', V: '🇻', W: '🇼', X: '🇽', Y: '🇾', Z: '🇿'
 }
-/* eslint-enable object-property-newline */
 
 // assumes regionCode is /^[A-Z]{2}$/
 export const getFlagEmoji = (regionCode: string): string => {
