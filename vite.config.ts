9dc93fa7a2a6846b2577b2b1b53c55b4b79e021f
Marc Abonce Seguin <marc-abonce@mailbox.org>
2023-10-25 21:57:04 -0600

    add copyleft notice to the bundled output

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import license from 'rollup-plugin-license'
+
+const licenseBanner = `
+React Lang Selecta
+@copyright 🄯 2023 Marc Abonce Seguin
+@license LiLiQ-R-1.1
+`
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +24,14 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
+      plugins: [
+        license({
+          banner: {
+            commentStyle: 'ignored',
+            content: licenseBanner,
+          },
+	}),
+      ],
     }
   }
 })
