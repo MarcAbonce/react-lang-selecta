cf8629d74fd3d8446abf20826f2563e33e896742
Marc Abonce Seguin <marc-abonce@mailbox.org>
2024-11-18 23:32:51 -0600

    add more languages from Ethnologue
    Requires manually downloading LanguageCodes.tab from:
    https://www.ethnologue.com/codes
    And saved into py-src/data
    
    And yes, that Python code is ugly. 🦆

--- py-src/build_locale_regions_json.py
+++ py-src/build_locale_regions_json.py
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: LiLiQ-Rplus-1.1 ⚜
 # License-Filename: LICENCE.txt
 
-import re
+import csv
 import json
+import re
 
 from babel import Locale, languages, localedata
-# from requests import get
 
 
 int_locale = Locale('ia')
@@ -14,15 +14,19 @@ locale_regions = {}
 unofficial_locale_regions = {}
 
 
-def add_locale_to_dict(lang, region, locale_dict):
+def add_locale_to_dict(lang, region, locale_dict, fallback_name=None):
     if lang in locale_dict:
         if region not in locale_dict[lang]['regions']:
             locale_dict[lang]['regions'].append(region)
     else:
-        name = int_locale.languages.get(lang)
+        name = int_locale.languages.get(lang) or ''
         if not name:
-            name = Locale(lang).language_name
+            if fallback_name:
+                name = fallback_name
         locale_dict[lang] = {'name': name.title(), 'regions': [region]}
+
+    if fallback_name and not locale_dict[lang]['name']:
+        locale_dict[lang]['name'] = fallback_name
 
 
 for locale in localedata.locale_identifiers():
@@ -39,7 +43,7 @@ for locale in localedata.locale_identifiers():
         else:
             add_locale_to_dict(lang, region, unofficial_locale_regions)
     elif locale_parts[-1] == '001':
-        # add United Nations for languages like Esperanto and Yiddish
+        # add United Nations for languages like Esperanto
         add_locale_to_dict(locale_parts[0], 'UN', locale_regions)
 
 
@@ -47,6 +51,17 @@ for locale in localedata.locale_identifiers():
 for lang in unofficial_locale_regions:
     if lang not in locale_regions:
         locale_regions[lang] = unofficial_locale_regions[lang]
+
+
+# get more language codes and names from Ethnologue
+with open('./data/LanguageCodes.tab') as ethnologue_file:
+    ethnologue_table = csv.reader(ethnologue_file, delimiter='\t')
+    next(ethnologue_table)  # skip header row
+    for lang_id, country_id, _, english_name in ethnologue_table:
+        if lang_id not in locale_regions:
+            add_locale_to_dict(
+                lang_id, country_id, locale_regions, fallback_name=english_name
+            )
 
 
 # sort regions so each run returns same output
